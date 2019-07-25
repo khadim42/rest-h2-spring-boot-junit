35251bf2ac33ffddf10556ac150be52255984cac
khadim.hussain <khadim.hussain@360factors.com>
2019-07-25 23:15:20 +0500

    master brnch

--- src/main/java/com/kh/embl/Myapplication.java
+++ src/main/java/com/kh/embl/Myapplication.java
@@ -8,6 +8,7 @@ public class Myapplication {
 
 	public static void main(String[] args) {
 		System.out.println("This is master branch");
+        System.out.println("This is master branch test 2");
 		SpringApplication.run(Myapplication.class, args);
 	}
 }
